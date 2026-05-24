05dc9dc05295552447e43d3d9f893f598a769878
Sansrus <ms928p@gmail.com>
2026-05-24 21:17:11 +0300

    remove leftover template files from merge

--- src/client/java/org/example/sansrus/sansrusmod/client/chatcoord/ChatPipelineFlags.java
+++
@@ -1,8 +0,0 @@
-package org.example.sansrus.sansrusmod.client.chatcoord;
-
-public class ChatPipelineFlags {
-    public static boolean coordReinjecting = false;
-    public static boolean deathMessageActive = false;
-
-}
-
--- src/client/java/org/example/sansrus/sansrusmod/client/maptip/MapTooltipData.java
+++
@@ -1,7 +0,0 @@
-package org.example.sansrus.sansrusmod.client.maptip;
-
-import net.minecraft.component.type.MapIdComponent;
-import net.minecraft.item.map.MapState;
-import net.minecraft.item.tooltip.TooltipData;
-
-public record MapTooltipData(MapIdComponent mapId, MapState state) implements TooltipData {}
